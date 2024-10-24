3f32b9e9206eaae5e53a397d2c1d95438a587181
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-10-24 14:37:15 +0300

    upload files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // domains: ["s3-inventorymanagement.s3.us-east-2.amazonaws.com"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        port: "", // Leave blank for default ports
+        pathname: "/**", // Allows any path in this domain
+      },
+    ],
+  },
+};
 
 export default nextConfig;
